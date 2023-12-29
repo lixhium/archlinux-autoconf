3b0f6faef91afa6d8a688459f6314454807cbf5b
lixhium <notkoishi@gmail.com>
2023-12-29 13:07:55 +0800

    added the software I need

--- stage2.bash
+++ stage2.bash
@@ -8,4 +8,10 @@ mv ./conf/nvim ~/.config/.
 mv ./conf/wm/ ~/Documents/.
 mv ./conf/.xinitrc ~/.
 
+rm -rf ./conf
+
+echo "installing all the software I need"
+pacman -S pipewire pipewire-pulse wireplumber pavucontrol xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xf86-video-amdgpu\
+    mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon noto-fonts nerd-fonts terminus-font wget curl htop neofetch
+
 echo "compile the dwm yourself, the dwm was in the ~/Documents/wm directory"
